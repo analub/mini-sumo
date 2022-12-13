8e75efa862c2b9910fd07cfc83ad6867f0186f12
Ana Luisa <ana.basilio@engenharia.ufjf.br>
2022-12-12 22:19:42 -0300

    correção na leitura do sensor digital

--- mini/src/digital_sensor.cpp
+++ mini/src/digital_sensor.cpp
@@ -4,22 +4,23 @@
 
 using namespace std;
 
-  DigitalSensor :: DigitalSensor(int pin){        //construtor
+  DigitalSensor :: DigitalSensor(int pin)
+  {
     this-> pin = pin;
   };
 
-  int DigitalSensor :: readSensor(){          //leitura do sensor, inimigo perto
+  int DigitalSensor :: readSensor(){                //leitura do sensor, inimigo perto
     this -> raw_reading = digitalRead(this->pin);
     
-    if(this-> raw_reading <2000)
+    if(this-> raw_reading==1)
       this->enemy_close = true;
     else 
       this->enemy_close = false;
     
-    if (enemy_close == 1)    
-      return true;
+    if (this->enemy_close == 1)    
+      return this->enemy_close;
     else
-      return false;   
+      return 0;   
   }
 
   /*
