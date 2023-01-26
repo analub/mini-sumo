1b0f7aeb5c644e83f032fdc9301d488c11ba342d
Ana Luisa <ana.basilio@engenharia.ufjf.br>
2023-01-26 02:12:23 -0300

    implementando move

--- src/strategy.cpp
+++ src/strategy.cpp
@@ -4,4 +4,12 @@
 
 using namespace std;
 
+  Move :: Move(int left_motor_power, int right_motor_power, int time_ms){
+    this->left_motor_power = left_motor_power;
+    this->right_motor_power = right_motor_power;
+    this->time_ms = time_ms;
+  }
+
+
+
   
